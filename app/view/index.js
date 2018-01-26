f10d544e84dd7312e684f6ded262330b55ded49a
Isnur Muhammad Suryo Margono <isnur_m_s_margono@smltech.co.id>
2018-01-26 15:47:58 +0700

    Update dashboard

--- app/view/index.js
+++ app/view/index.js
@@ -5,7 +5,6 @@
 
 Ext.define('DASHBOARD.view.index', {
   extend: 'Ext.panel.Panel',
-  title: '<table style="text-align: center; border-collapse: collapse; width:100%"><tr><td><img src="assets/images/Isnur.com.png" style="padding:  5px; width: 111px;"></td><td align="left" style="padding-left: 10px;"><b><font size="5">DASHBOARD</font><font id="tahun" size="5" color="#ed1313"></font></b></td></tr></table>',
   xtype: 'index',
   itemId: 'index',
   layout : {
@@ -14,26 +13,55 @@ Ext.define('DASHBOARD.view.index', {
     align: 'stretch'
   },
   items: [{
+    region: 'north',
+    xtype: 'container',
+    style: 'margin: 0px; padding: 0px;',
+    height: 40,
+    html: '<table style="text-align: center; border-collapse: collapse; width:100%"><tr><td width="100px"><img src="assets/images/Isnur.com.png" style="padding: 5px; width: 100%;"></td><td align="left" style="padding-left: 10px;"><b><font size="5" color="#374da1">DASHBOARD </font><font id="tahun" size="5" color="#ed1313"></font></b></td></tr></table>',
+    listeners: {
+      afterrender: function () {
+        var tahun = new Date().getFullYear();
+        $("#tahun").html(tahun);
+      }
+    }
+  },{
     title: 'South Region is resizable',
     region: 'south',
-    xtype: 'panel',
-    height: 100,
-    split: true,
-    margin: '0 5 5 5'
+    xtype: 'container',
+    height: 30,
+    style: 'width: 100%; text-align: center; padding: 7px;',
+    html: 'Copyright <a href="http://isnur.com">Isnur.com</a>'
   }, {
-    title: 'West Region is collapsible',
+    title: 'MENU',
     region:'west',
     xtype: 'panel',
-    margin: '5 0 0 5',
-    width: 200,
+    bodyStyle: 'border: none; background-color: #fafafa;',
+    width: 150,
     collapsible: true,
     id: 'west-region-container',
-    layout: 'fit'
+    layout: 'fit',
+    items: [{
+      xtype: 'menu',
+      width: 100,
+      plain: true,
+      floating: false,
+      id: 'menuId',
+      items: [{
+        text: 'Button',
+      }, {
+        text: 'Chart'
+      }, {
+        text: 'Form'
+      }, {
+        text: 'Grid'
+      }, {
+        text: 'Layout'
+      }]
+    }]
   }, {
-    title: 'Center Region',
     region: 'center',
     xtype: 'panel',
-    layout: 'fit',
-    margin: '5 5 0 0'
+    bodyStyle: 'border: none;',
+    layout: 'fit'
   }]
 });
--- assets/script/app.js
+++ assets/script/app.js
@@ -2,7 +2,3 @@
  * @author Isnur MSM
  *
  */
-$(document).ready(function(){
-  var tahun = new Date().getFullYear();
-  $("#tahun").html(tahun);
-});
